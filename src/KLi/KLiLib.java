7f8fea1d7a028656f98d767a0dc903103a709e3d
Kent <kli8661@bths.edu>
2018-09-18 10:14:30 -0400

    3 Things Done

--- src/KLi/KLiLib.java
+++ src/KLi/KLiLib.java
@@ -6,4 +6,44 @@ public class KLiLib {
     {
         System.out.println(str);
     }
+
+    public static String isPalindrome(String str)
+    {
+        String reversedStr = "";
+        for(int i = str.length(); i > 0; i--)
+        {
+            String letter = str.substring(i-1,i);
+            reversedStr += letter;
+        }
+        if(reversedStr.equalsIgnoreCase(str))
+        {
+            return "true";
+        }
+        else
+        {
+            return "false";
+        }
+    }
+
+    public static String dateStr(String str)
+    {
+        String str1 = str.substring(0,2);
+        String str2 = str.substring(3,5);
+        String str3 = str.substring(6,str.length());
+        return str2 + "-" + str1 + "-" + str3;
+    }
+
+    public static String sumUpTo(int range)
+    {
+        if(range < 0)
+            return "false";
+        else {
+            int sumOfNumbers = 0;
+            for (int i = 0; i <= range; i++) {
+                sumOfNumbers += i;
+            }
+            String stringOfNum = Integer.toString(sumOfNumbers);
+            return stringOfNum;
+        }
+    }
 }
--- src/KLi/TestFile.java
+++ src/KLi/TestFile.java
@@ -1,11 +1,28 @@
 package KLi;
 
-import static KLi.KLiLib.println;
+import static KLi.KLiLib.*;
 
 public class TestFile {
 
     public static void main(String[] args)
     {
+        //Test
         println("I love a cup of Java in the morning.");
+
+        //Testing palindrome if it's correct
+        println(isPalindrome("racecar"));
+
+        //Testing palindrome if the string is false
+        println(isPalindrome("apple"));
+
+        //Testing birthday string
+        println(dateStr("09/18/2001"));
+
+        //Testing range for sumUpTo
+        println(sumUpTo(10));
+
+        //Testing if range is negative
+        println(sumUpTo(-10));
     }
+
 }
