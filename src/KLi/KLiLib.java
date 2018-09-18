0b9cb40346745f40d77fa1891d84150c597e97df
BT_1N3_19@bktech.local <BT_1N3_19@bktech.local>
2018-09-18 09:16:38 -0400

    Initial commit

---
+++ src/KLi/KLiLib.java
@@ -0,0 +1,9 @@
+package KLi;
+
+public class KLiLib {
+
+    public static void println(String str)
+    {
+        System.out.println(str);
+    }
+}
---
+++ src/KLi/TestFile.java
@@ -0,0 +1,11 @@
+package KLi;
+
+import static KLi.KLiLib.println;
+
+public class TestFile {
+
+    public static void main(String[] args)
+    {
+        println("I love a cup of Java in the morning.");
+    }
+}
